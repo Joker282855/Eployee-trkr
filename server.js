42d767bb7c17bb72afee9901a528926e704b5c8d
unknown <jtjackson325@gmail.com>
2021-09-17 16:12:34 -0600

    got the routes folder file and structure setup

--- server.js
+++ server.js
@@ -6,20 +6,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'Coderrocks12!',
-        database: 'tracker'
-    },
-    console.log('Now connected to the tracker database')
-);
-
-db.query(`SELECT * FROM department`, (err, rows) => {
-    console.log(rows);
-});
+app.use(require('./routes'));
 
 app.use((req, res) => {
     res.status(404).end();
