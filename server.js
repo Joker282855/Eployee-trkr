3f611da301eadd1958d57b57400e523a1004ce20
unknown <jtjackson325@gmail.com>
2021-09-17 18:41:05 -0600

    got the majoirty of the routes typed out

--- server.js
+++ server.js
@@ -6,7 +6,72 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(require('./routes'));
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Coderrocks12!',
+        database: 'tracker'
+    },
+    console.log('Now connected to the tracker database')
+);
+
+// view all the departments
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// view all roles
+app.get('/api/role', (req, res) => {
+    const sql = `SELECT * FROM role`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'roles retrieved',
+            data: rows
+        });
+    });
+});
+
+// view all employees
+app.get('/api/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'employees retrieved',
+            data: rows
+        });
+    });
+});
+
+// add a department
+app.post('/api/department', ({ body}, res) => {
+    const errors = inputCheck(body, 'name');
+    if (errors) {
+        res.status(400).json({ errors: errors });
+    }
+});
 
 app.use((req, res) => {
     res.status(404).end();
