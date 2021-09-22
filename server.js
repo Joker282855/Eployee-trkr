7ccd5d357d0d27fc027ee9f93effce84f85a3611
unknown <jtjackson325@gmail.com>
2021-09-22 17:01:42 -0600

    got the code for the put route initally started

--- server.js
+++ server.js
@@ -141,8 +141,29 @@ app.post('/api/employee', ({ body }, res) => {
     });
 });
 
-// update an employee role
-
+// // update an employees role
+// app.put('api/employee/:id', (req, res) => {
+//     const sql = `UPDATE employee SET role_id = ?
+//                 WHERE id ?`;
+//     const params = [req.body.role_id, req.params.id];
+    
+//     db.query(sql, params, (err, result) => {
+//         if (err) {
+//             res.status(400).json({ error: err.message });
+//             // check if a record was found
+//         } else if (!result.affectedRows) {
+//             res.json({
+//                 message: 'Employee not found'
+//             });
+//         } else {
+//             res.json({
+//                 message: 'success',
+//                 data: req.body,
+//                 changes: result.affectedRows
+//             });
+//         }
+//     });
+// });
 
 app.use((req, res) => {
     res.status(404).end();
