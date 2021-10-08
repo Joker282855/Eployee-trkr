2e2e3246587355bffcb89d8c269703e34a157489
unknown <jtjackson325@gmail.com>
2021-10-08 09:20:26 -0600

    got the inital part of the inquirer prompts set up for a switch case

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const inquirer = require('inquirer');
 
 function menu(){
     inquirer.prompt([{
-        type: 'list',
-        name: 'result',
-        message: 'What would you like to do',
+        type: "list",
+        name: "result",
+        message: "What would you like to do",
         choices: [{
-            name: 'View All Departments',
-            value: 'viewAllDepartments'
+            name: "View All Departments",
+            value: "viewAllDepartments"
         },{
-            name: 'View All Roles'
+            name: "View All Roles",
+            value: "viewAllRoles"
+        },{
+            name: "View All Emmployees",
+            value: 'viewAllEmployees'
+        },{
+            name: "Add a Department",
+            value: "addADepartment"
+        },{
+            name: "Add a Role",
+            value: "addARole"
+        },{
+            name: "Add an Employee",
+            value: "addAnEmployee"
+        },{
+            name: "Update an Employee Role",
+            value: "updateAnEmployeeRole"
         }]
     }])
 }
