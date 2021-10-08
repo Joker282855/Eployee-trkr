4b857c6365829ac1e09b6c46de7f7e1a34e34d24
unknown <jtjackson325@gmail.com>
2021-10-08 09:36:08 -0600

    got the switch case sorted out

--- server.js
+++ server.js
@@ -27,6 +27,35 @@ function menu(){
         },{
             name: "Update an Employee Role",
             value: "updateAnEmployeeRole"
+        },{
+            name: "quit",
+            value: "quit"
         }]
-    }])
+    }]).then(function(data){
+        console.log(data)
+        switch (data.result){
+            case "viewAllDepartments":
+                viewDepartment()
+                break;
+            case "viewAllRoles":
+                viewRoles()
+                break;
+            case "viewAllEmployees":
+                viewEmployee()
+                break;
+            case "addADepartment":
+                addDepartment()
+                break;
+            case "addARole":
+                addRole()
+                break;
+            case "addAnEmployee":
+                addEmployee()
+                break;
+            case "updateAnEmployeeRole":
+                employeeUpdate()
+                break;
+        }
+    })
 }
+
